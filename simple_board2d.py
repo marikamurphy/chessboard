1104fa3bf42f018c3fbc1fa6bb4547a58436f25c
mm89039 <marikamurphy18@gmail.com>
2019-05-07 16:18:00 -0500

    proper random rotation

--- simple_board2d.py
+++ simple_board2d.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R
 from mpl_toolkits.mplot3d import Axes3D
 import math
+import random
 
 import sys
 ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
@@ -91,36 +92,43 @@ def plot_board_2d(board, marker):
 	plt.plot(board[0,:], board[1,:], marker)
 	plt.grid(True)
 
-		
+def random_trans_generator():
+	rand1 = random.random()*math.pi
+	rand2 = random.random()*math.pi
+	rand3 =	random.random()*math.pi
+	
+	return rot_mat(rand1,rand2,rand3)		
 
 if __name__ == '__main__':
 
 	num_pnts = 5
  
 	board3D = create_board3D(num_pnts)
-	board_3Dcart = hom_cart_trans(board3D)
-	print("board3D")
-	print(board3D)
-	print("board_3Dcart")
-	print(board_3Dcart)
+	#board_3Dcart = hom_cart_trans(board3D)
+	#print("board3D")
+	#print(board3D)
+	#print("board_3Dcart")
+	#print(board_3Dcart)
 	plot_board_2d(hom_cart_trans(board3D), 'ro')
 	board2D = create_board2D(num_pnts)
 	board_2Dcart = hom_cart_trans(board2D)
-	print("board2D")
-	print(board2D)
-	print("board_2Dcart")
-	print(board_2Dcart)
+	#print("board2D")
+	#print(board2D)
+	#print("board_2Dcart")
+	#print(board_2Dcart)
+
 	print("rigid transformation")
-	rot_mat = rot_mat(math.pi,0.0,math.pi)
+	rot_mat = random_trans_generator() #arbitrary rotation
 	rigid_trans_mat = rigid_trans(rot_mat, [[0],[0], [0]])
 	trans_mat = transform_matrix(board3D, rigid_trans_mat)
 	plot_board_2d(hom_cart_trans(trans_mat), 'b*')
+	
 	print(hom_cart_trans(trans_mat))
 
 	board2D = hom_3Dto2D(trans_mat)
-	print("trans_mat")
-	print(trans_mat)
-	print("board2D")
-	print(board2D)
+	#print("trans_mat")
+	#print(trans_mat)
+	#print("board2D")
+	#print(board2D)
 	plt.show()
 
