631d219615f3cd8b8b6f4d2892fe9efbb4624d98
Roger Zhong <rzhong688@gmail.com>
2019-05-18 15:03:40 -0500

    finds chessboard

--- chessboardPoints.py
+++ chessboardPoints.py
@@ -1,6 +1,11 @@
-import numpy as np
-import cv2
+import math
 import glob
+import webbrowser
+import argparse
+import sys
+import numpy as np
+import sys
+import cv2
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -13,25 +18,92 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('*.jpg')
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
+# orb = cv2.ORB_create()
 
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
+# #images = glob.glob('board1.jpg')
 
-        # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv2.imshow('img',img)
-        cv2.waitKey(500)
+
+#clahe = cv2.createCLAHE();
+#clahe.apply(img, img)
+#corners
+
+# kp_grayframe = orb.detect(gray, None)
+
+# # Find the chess board corners
+
+
+# img2 = cv2.drawKeypoints(img,kp_grayframe, img)
+
+# cv2.imshow('img',img2)
+
+# If found, add object points, image points (after refining them)
+#
+
+    # Draw and display the corners
+
+
+#corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+#imgpoints.append(corners2)
+
+#(thresh, im_bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+DEFAULT_WIDTH = 500
+img = cv2.imread('board8.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+thresh = 127
+im_bw = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)[1]
+cv2.imshow("bw", im_bw)
+
+height, width = gray.shape[:2]
+
+scaleFactor0 = DEFAULT_WIDTH/width
+
+gray1 = cv2.resize(im_bw, None, fx=scaleFactor0, fy=scaleFactor0, interpolation = cv2.INTER_LINEAR)
+img1 = cv2.resize(img, None, fx=scaleFactor0, fy=scaleFactor0, interpolation = cv2.INTER_LINEAR)
+dimensions = (7,7)
+ret, corners = cv2.findChessboardCorners(gray1, dimensions , None)
+print(corners)
+cv2.drawChessboardCorners(img1, dimensions, corners , ret)
+cv2.imshow('img1',img1)
+
+cv2.waitKey(10000)
 
 cv2.destroyAllWindows()
+# Find the chess board corners
+
+
+# ret, corners = cv2.findChessboardCorners(gray, (6,6) ,None)
+
+# # If found, add object points, image points (after refining them)
+# if ret == True:
+#     #objpoints.append(objp)
+
+#     corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+#     #imgpoints.append(corners2)
+
+#     # Draw and display the corners
+#     img2 = cv2.drawChessboardCorners(img, (6,6), corners2,ret)
+#     cv2.imshow('img',img2)
+    
+
+# for fname in images:
+#     img = cv2.imread(fname)
+#     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#     # Find the chess board corners
+#     ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+
+#     # If found, add object points, image points (after refining them)
+#     if ret == True:
+#         objpoints.append(objp)
+
+#         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+#         imgpoints.append(corners2)
+
+#         # Draw and display the corners
+#         img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+#         cv2.imshow('img',img)
+
+
